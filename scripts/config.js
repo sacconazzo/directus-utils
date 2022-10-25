f8eccb5323a84ea41b5ef8658c948bb682f6ab3a
Giona Righini <grighini@alteanet.it>
2022-10-25 10:53:05 +0200

    fix port db

--- scripts/config.js
+++ scripts/config.js
@@ -7,7 +7,7 @@ module.exports = {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: Number(process.env.DB_PORT),
     },
   },
 }
