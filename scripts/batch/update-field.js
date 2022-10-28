2ff2723e05024b55b28f05acfbed1c832083d9eb
Giona Righini <sapbasis@alteanet.it>
2022-10-28 13:29:32 +0200

    log

--- scripts/batch/update-field.js
+++ scripts/batch/update-field.js
@@ -14,7 +14,9 @@ module.exports = async (table, field, condition, data) => {
         ? await knex(table).where(knex.raw(condition)).update(field, update)
         : await knex(table).update(field, update)
 
-    console.error(`Table '${table}' updated with ${n} records`)
+    console.log(`Table '${table}' updated with ${n} records`)
+
+    console.warn('Remember to generate a new snapshot')
   } catch (err) {
     console.error(err.message)
   } finally {
