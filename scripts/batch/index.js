6bfaa2cc3593ae55dad0e435d289958373d38ec3
Giona Righini <grighini@alteanet.it>
2022-11-01 09:16:10 +0100

    added table for batch

--- scripts/batch/index.js
+++ scripts/batch/index.js
@@ -4,6 +4,12 @@ const entities = {
   fields: 'directus_fields',
   folders: 'directus_folders',
   permissions: 'directus_permissions',
+  users: 'directus_users',
+  settings: 'directus_settings',
+  roles: 'directus_roles',
+  presets: 'directus_presets',
+  files: 'directus_files',
+  collections: 'directus_collections',
 }
 
 module.exports = {
